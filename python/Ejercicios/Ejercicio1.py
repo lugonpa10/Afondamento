e1a0dbb8b482aca676522e45f3ff282779dcea7e
lugonpa10 <lugonpa10@gmail.com>
2025-10-23 12:55:56 +0200

    changes

--- python/Ejercicios/Ejercicio1.py
+++ python/Ejercicios/Ejercicio1.py
@@ -5,6 +5,18 @@ print("2-Potencia")
 print("3-Rango")
 print("4-Listas")
 
+if opcion == 1:
+    num1 = int(input("Introduce el dividendo"))
+    num2 = int(input("Introduce el divisor"))
+    if num2 == 0:
+        print("El divisor no puede ser 0")
+        num2 == int(input("Introduce un numero valido"))
+    print("Cociente: ")
+    
+
+
+       
+    
 
     
 
