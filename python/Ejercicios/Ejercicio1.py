9a931255f99b3d0c5ac734edddd180f462aa5bfb
lugonpa10 <lugonpa10@gmail.com>
2025-10-24 10:46:09 +0200

    changes

--- python/Ejercicios/Ejercicio1.py
+++ python/Ejercicios/Ejercicio1.py
@@ -1,4 +1,4 @@
-import math
+import random
 
 def divisiones():
     num1 = float(input("Introduce el dividendo: "))
@@ -16,14 +16,48 @@ def potencia():
      print(f"La base {base} con exponente {exponente} da como resultado: { base**exponente:.2f}")
 
 def rango():
-    
+    cont = 0
     num1 = int(input("Introduce el primer numero: "))
     num2 = int(input("Introduce el segundo numero: "))
     if num1 > num2:
-         print(f"{num1} es mayor que {num2}\n")
-       
+
+         for i in range(num2,num1 + 1):
+              cont+=i
+         print(f"{num1} es mayor que {num2} y la suma de todos los numeros entre ellos es {cont}\n")
+
+    elif num1 == num2:
+         print("Los numeros son iguales")   
+
     else:
-         print(f"{num2} es mayor que {num1}\n")
+         for i in range(num1,num2 +1):
+            cont+=i
+         print(f"{num2} es mayor que {num1} y la suma de todos los numeros entre ellos es {cont}\n")
+            
+def listas():
+     tamanho =int(input("Dime el tamaño para la lista 1: "))
+     lista1=[]
+     tamanho =int(input("Dime el tamaño para la lista 2: "))
+     lista2=[]
+
+     for _ in range(tamanho):
+          
+          
+          
+
+   
+
+    
+
+
+
+    
+    
+            
+              
+    
+         
+
+
          
    
 
@@ -49,6 +83,8 @@ elif opcion == 2:
      potencia()
 elif opcion == 3:
      rango()
+elif opcion == 4:
+     
      
      
      
