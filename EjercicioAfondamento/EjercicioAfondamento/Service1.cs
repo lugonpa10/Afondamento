2dcba708b0a1e154cc66514e0ce6e8103e50ffba
lugonpa10 <lugonpa10@gmail.com>
2026-01-28 11:43:14 +0100

    changes

---
+++ EjercicioAfondamento/EjercicioAfondamento/Service1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EjercicioAfondamento
+{
+    public partial class ServidorAfondamento : ServiceBase
+    {
+        public ServidorAfondamento()
+        {
+            InitializeComponent();
+            string nombre = "ServidorAfondamento";
+            string logDestino = "Application";
+            if (!EventLog.SourceExists(nombre))
+            {
+                EventLog.CreateEventSource(nombre, logDestino);
+            }
+        }
+
+        public void WriteEvent(string mensaje)
+        {
+            const string nombre = "ServidorAfondamento";
+            EventLog.WriteEntry(nombre, mensaje);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteEvent("Iniciando el servidor");
+            Socket client = s.Accept();
+            Thread hilo = new Thread(() => ClientDispatcher(client));
+            hilo.IsBackground = true;
+            hilo.Start();
+        }
+
+        protected override void OnStop()
+        {
+            WriteEvent("Deteniendo el servidor");
+
+        }
+
+       
+        public bool ServerRunning { set; get; } = true;
+        public int Port { set; get; } = 135;
+        public int[] puertosAlternativos = { 135, 135, 135, 31416 };
+        public bool puertoOcupado = true;
+        private Socket s;
+        private int i = 0;
+
+        public void initServer()
+        {
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, Port);
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                while (puertoOcupado)
+                {
+                    try
+                    {
+                        puertoOcupado = false;
+                        s.Bind(ie);
+                    }
+                    catch (SocketException e) when (e.ErrorCode == 10048)
+                    {
+                        puertoOcupado = true;
+                        ie.Port = puertosAlternativos[i];
+                        i++;
+                    }
+
+                }
+                Console.WriteLine($"Puerto {ie.Port} libre");
+
+                s.Listen(10);
+                Console.WriteLine($"Se ha iniciado el servidor" + $" Escuchando en {ie.Address}:{ie.Port}");
+
+                while (ServerRunning)
+                {
+                    Socket client = s.Accept();
+                    Thread hilo = new Thread(() => ClientDispatcher(client));
+                    hilo.IsBackground = true;
+                    hilo.Start();
+
+
+                }
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Puertos ocupados");
+            }
+            catch (SocketException e) when (e.ErrorCode == 10048)
+            {
+                Console.WriteLine($"Puerto {Port} en uso");
+
+            }
+            catch (SocketException)
+            {
+
+                Console.WriteLine("Fin del servidor");
+
+            }
+
+        }
+        private void ClientDispatcher(Socket sClient)
+        {
+            using (sClient)
+            {
+                IPEndPoint ieClient = (IPEndPoint)sClient.RemoteEndPoint;
+                Console.WriteLine($"Cliente conectado: {ieClient.Address}" +
+                    $" en puerto {ieClient.Port}");
+
+                Encoding codificacion = Console.OutputEncoding;
+                using (NetworkStream ns = new NetworkStream(sClient))
+                using (StreamWriter sw = new StreamWriter(ns))
+                using (StreamReader sr = new StreamReader(ns))
+                {
+                    sw.AutoFlush = true;
+
+                    string? opcion = "";
+                    sw.WriteLine("Bienvenido a mi servidor,introduce un comando");
+                    {
+                        try
+                        {
+                            opcion = sr.ReadLine();
+                            if (opcion != null && (opcion == "close" || opcion.StartsWith("close ")))
+                            {
+                                string programData = Environment.GetEnvironmentVariable("PROGRAMDATA");
+                                string archivo = "password.txt";
+                                string rutaArchivo = programData + "\\" + archivo;
+                                string contraseñaCorrecta;
+                                try
+                                {
+                                    using (StreamReader sr2 = new StreamReader(rutaArchivo))
+                                    {
+                                        contraseñaCorrecta = sr2.ReadLine().Trim();
+
+                                    }
+                                    string[] contraseña = opcion.Split(" ");
+
+                                    if (contraseña.Length < 2)
+                                    {
+                                        sw.WriteLine("No se ha enviado la contrasenha");
+                                    }
+                                    else if (contraseña[1] != contraseñaCorrecta)
+                                    {
+                                        sw.WriteLine("La contrasenha es incorrecta");
+
+                                    }
+                                    else
+                                    {
+                                        sw.WriteLine("Contrasenha Correcta");
+                                        StopServer();
+                                    }
+                                }
+                                catch (FileNotFoundException e)
+                                {
+                                    sw.WriteLine($"No se encontro el archivo: '{e}'");
+                                }
+                                catch (UnauthorizedAccessException e)
+                                {
+                                    sw.WriteLine($"No tienes los permisos necesarios: '{e}'");
+                                }
+                                catch (DirectoryNotFoundException e)
+                                {
+                                    sw.WriteLine($"No se pudo encontrar el directorio: '{e}'");
+                                }
+                                catch (IOException e)
+                                {
+                                    sw.WriteLine($"No se pudo abrir el archivo: '{e}'");
+                                }
+                            }
+                            else if (opcion != "time" && opcion != "date" && opcion != "all")
+                            {
+                                sw.WriteLine("El comando no es valido");
+                            }
+                            else
+                            {
+                                switch (opcion)
+                                {
+                                    case "time":
+                                        sw.WriteLine(DateTime.Now.ToString("T"));
+
+                                        break;
+                                    case "date":
+                                        sw.WriteLine(DateTime.Now.ToString("d"));
+
+                                        break;
+
+                                    case "all":
+                                        sw.WriteLine(DateTime.Now.ToString("G"));
+
+                                        break;
+                                }
+
+                            }
+                        }
+
+                        catch (IOException)
+                        {
+                            opcion = null;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    }
+}
